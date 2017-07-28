b214a3e31abd5b7e031c873364bff19143e5f799
Shashikanth.p <Shashikanth.p@LP-CNU3229BCB.HCLT.CORP.HCL.IN>
2017-07-28 13:18:50 +0530

    Added Reousrce code

--- src/main/java/com/hcl/loan/model/exception/ResourceNotFoundException.java
+++ src/main/java/com/hcl/loan/model/exception/ResourceNotFoundException.java
@@ -1,5 +1,10 @@
 package com.hcl.loan.model.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
+	
+	public ResourceNotFoundException(String message)
+	{
+		super(message);
+	}
 
 }
