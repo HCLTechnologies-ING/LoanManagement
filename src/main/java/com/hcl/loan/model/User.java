f3651120f42ede47d1b29ec53c1cc989b632388c
sasikumar_g <sasikumar_g@LP-CNU3229BNL.HCLT.CORP.HCL.IN>
2017-08-02 19:15:17 +0530

    Serial version id changed

--- src/main/java/com/hcl/loan/model/User.java
+++ src/main/java/com/hcl/loan/model/User.java
@@ -12,7 +12,7 @@ import org.hibernate.validator.constraints.Email;
 
 public class User implements Serializable {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 12L;
 	
 	
 	private String userId;
