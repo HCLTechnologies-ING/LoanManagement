bd1d0942a4b9cf57e51e8c29de25b531be5f1077
vivek <softvivek@yahoo.com>
2017-08-03 12:00:45 +0530

    Update LoanDisbursmentServiceImpl.java

--- src/main/java/com/hcl/loan/service/LoanDisbursmentServiceImpl.java
+++ src/main/java/com/hcl/loan/service/LoanDisbursmentServiceImpl.java
@@ -1,40 +1,94 @@
-package com.hcl.loan.service;
+ package com.hcl.loan.service;
+
+
 
 import java.util.ArrayList;
+
 import java.util.List;
 
+
+
+import org.apache.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+
 import org.springframework.transaction.annotation.Transactional;
 
+
+
 import com.hcl.loan.dao.LoanDisbursmentDao;
+
+import com.hcl.loan.dao.LoanDisbursmentDaoImpl;
+
 import com.hcl.loan.model.Loan;
+
 import com.hcl.loan.model.LoanDisbursment;
+
 import com.hcl.loan.model.User;
 
-@Service("loanService")
-@Transactional
+
+
+@Service
+
 public class LoanDisbursmentServiceImpl implements LoanDisbursmentService {
 
-                @Autowired
-                private LoanDisbursmentDao loanDisbursmentDao;
+
+
+        
+
+				
+
+	
+
+	private static final Logger logger = Logger.getLogger(LoanDisbursmentServiceImpl.class);
+
+
+
+	
+
+	
+
+	@Autowired
+
+    private LoanDisbursmentDao loanDisbursmentDao;
+
+
 
               
+
                 /*
+
                 *
+
                 */
+
                 public void updateLoanDisbursmentDetails(LoanDisbursment loanDisb) {
+
                 	loanDisbursmentDao.save(loanDisb);
+
                 }
 
+
+
                 public List<Loan> findAllApprovedLoans() {
+
                 	
-                	List appLoan = new ArrayList<Loan>();
+
+                	List<Loan> appLoan = new ArrayList();
+
                 	
+
                 	appLoan =loanDisbursmentDao.findApprovedLoanDeatils();
+
                 	               	
+
                 	return appLoan;
+
                 }
+
+
 
 }
 
