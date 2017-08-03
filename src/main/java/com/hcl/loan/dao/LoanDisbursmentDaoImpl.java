003540910977c33f57cbfd6593db20fc91fc04ab
vivek <softvivek@yahoo.com>
2017-08-03 12:02:41 +0530

    Update LoanDisbursmentDaoImpl.java

--- src/main/java/com/hcl/loan/dao/LoanDisbursmentDaoImpl.java
+++ src/main/java/com/hcl/loan/dao/LoanDisbursmentDaoImpl.java
@@ -1,72 +1,166 @@
-package com.hcl.loan.dao;
+ package com.hcl.loan.dao;
 
+
+
+　
 
 import java.sql.ResultSet;
+
 import java.sql.SQLException;
+
 import java.sql.Types;
+
 import java.util.ArrayList;
+
 import java.util.List;
+
+
 
 import javax.sql.DataSource;
 
+
+
 import org.apache.commons.logging.LogFactory;
+
 import org.apache.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
+
 import org.springframework.core.env.Environment;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+
 import org.springframework.jdbc.core.RowMapper;
+
 import org.springframework.stereotype.Repository;
+
+
+
+　
+
 import com.hcl.loan.model.Loan;
+
 import com.hcl.loan.model.LoanDisbursment;
 
+import com.hcl.loan.service.exception.ApprovedLoanNotFoundException;
 
+
+
+　
+
+　
 
  
-@PropertySource("classpath:scr/main/resources/sql.properties") 
-@Repository("LoanDisbursment")
+
+ 
+
+@Repository
+
 public class LoanDisbursmentDaoImpl implements LoanDisbursmentDao {
 
-	@Autowired
+
+
+	private static final Logger logger = Logger.getLogger(LoanDisbursmentDaoImpl.class);
+
+	
+
+	
+
 	JdbcTemplate jdbcTemplate;
+
      
+
     @Autowired
+
     Environment env;
 
+
+
     public LoanDisbursmentDaoImpl(DataSource aDataSource)
+
     {
+
     	jdbcTemplate =new JdbcTemplate(aDataSource);
+
     }
+
 	
+
 	public List<Loan> findApprovedLoanDeatils() {
+
        
-		List<Loan> loanDtl = new ArrayList<Loan>();
+
+		List<Loan> loanDtl =new ArrayList();
+
     	
+
        
+
     	
+
 		loanDtl  =  jdbcTemplate.query(env.getProperty("APPROVED_LOAN_QUERY"), new Object[] { },
+
     		   new RowMapper<Loan>()
+
     		   {
+
     	   			public Loan mapRow(ResultSet rs,int rownum) throws SQLException
+
     	   				{
-    	   				Loan lnDtl = new Loan();
-    	   				lnDtl.setLoanId(rs.getLong(0));
-    	   				return lnDtl;
+
+    	   					Loan lnDtl = new Loan();
+
+    	   					lnDtl.setLoanId(rs.getLong(1));
+
+    	   					return lnDtl;
+
     	   				}
+
     		   });
+
+		
+
+		if(loanDtl!= null && loanDtl.isEmpty())
+
+		{
+
+			throw new ApprovedLoanNotFoundException("No Approved Loans to Dispatch");
+
+		}
+
+		 
+
        return loanDtl;
+
        
+
     }
+
+
 
     public boolean save(LoanDisbursment loanDisb) 
+
     {
-    	jdbcTemplate.update(env.getProperty("LOAN_DISB_INSERT"),new Object[]{loanDisb.getDisbursmentDate(),loanDisb.getNumberOfChecks(),loanDisb.getDisbursmentMode(),loanDisb.getEmailNotification(),loanDisb.getAccountNumber()},
-    			new Object[]{Types.DATE,Types.INTEGER,Types.VARCHAR,Types.CHAR,Types.LONGNVARCHAR});
-    	    	
+
+     	
+
+    	jdbcTemplate.update(env.getProperty("LOAN_DISB_INSERT"),new Object[]{loanDisb.getLoanId(),loanDisb.getUserId(),"2017-08-02",loanDisb.getUserId(),"2017-08-02",loanDisb.getDisbursmentDate(),loanDisb.getNumberOfChecks(),loanDisb.getDisbursmentMode(),loanDisb.getEmailNotification(),loanDisb.getAccountNumber()},
+
+    			new int[]{Types.BIGINT,Types.BIGINT,Types.DATE,Types.BIGINT,Types.DATE,Types.DATE,Types.INTEGER,Types.VARCHAR,Types.CHAR,Types.LONGNVARCHAR});
+
+    	   	
+
+    	
+
     	return true;
+
     }
 
+
+
      
+
  
+
 }
 
